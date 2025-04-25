fd61f8a409b440bf085478567757b57f29d4dd64
HAPPYHOB\happy <capaboss111@naver.com>
2025-04-25 17:44:53 +0900

    새로운 환경과 파일 설정

--- config/database.py
+++
@@ -1,8 +0,0 @@
-from pymongo import MongoClient
-
-client = MongoClient("mongodb+srv://admin:test1234!@cluster0.wrrhuqj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-
-
-db = client.todo_db
-
-collection_name = db["todo_collection"]
