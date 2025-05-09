b18e6ea8ab646bbbffb5a428fc14e6a1756c4510
hobinLee <happyhob24@gmail.com>
2025-05-09 13:30:15 +0900

    fastapi- mysql 연동 실습 마무리

--- main.py
+++ main.py
@@ -62,6 +62,20 @@ async def create_post(post:PostBase, db: db_dependency):
     db.add(db_post)
     db.commit()    
 
+@app.get("/posts/{post_id}", status_code =status.HTTP_200_OK)
+async def read_post(post_id: int, db: db_dependency):
+    post = db.query(models.Post).filter(models.Post.id == post_id).first()
+    if post is None:
+        raise HTTPException(status_code = 404, detail = 'Post was not found')
+    return post
+
+@app.delete("/posts/{post_id}",status_code = status.HTTP_200_OK)
+async def delete_post(post_id: int, db: db_dependency):
+    db_post = db.query(models.Post).filter(models.Post.id ==post_id).first()
+    if db_post is None:
+        raise HTTPException(status_code=404, detaile='Post was not found')
+    db.delete(db_post)
+    db.commit()
 
 @app.post("/users/", status_code = status.HTTP_201_CREATED)
 async def create_user(user:UserBase, db: db_dependency):
