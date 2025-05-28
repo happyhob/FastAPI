c184ebb9d589f5d52dcc4914d2563ee173a36898
hobinLee <happyhob24@gmail.com>
2025-05-28 11:54:55 +0900

    url 변경

--- database.py
+++ database.py
@@ -3,9 +3,9 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
 
-URL_DATABASE ='sqlite:///./todosapp.db'
+SQLALCHEMY_DATABASE_URL ='sqlite:///./todosapp.db'
 
-engine = create_engine(URL_DATABASE)
+engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args = {'check_same_thread': False})
 
 '''bind : Sqlalchemy에서 Session이 어떤 DB엔진과 연결될지를 지정하는 매개변수'''
 SessionLocal  = sessionmaker(autocommit = False, autoflush=False, bind = engine)
